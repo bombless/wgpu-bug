c273895a469cf8c7149bcc691bfde601e3900e36
York Xiang <bombless@126.com>
2023-10-05 20:36:05 +0800

    Basic shape

---
+++ src/main.rs
@@ -0,0 +1,216 @@
+use app_surface::{AppSurface, SurfaceFrame};
+use std::iter;
+use utils::framework::{run, Action};
+use wgpu::ShaderStages;
+use wgpu::util::DeviceExt;
+use winit::{dpi::PhysicalSize, window::WindowId};
+
+mod uniform;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x2,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.0868241, 0.49240386],
+    }, // A
+    Vertex {
+        position: [-0.49513406, 0.06958647],
+    }, // B
+    Vertex {
+        position: [-0.21918549, -0.44939706],
+    }, // C
+    Vertex {
+        position: [0.35966998, -0.3473291],
+    }, // D
+    Vertex {
+        position: [0.44147372, 0.2347359],
+    }, // E
+];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
+
+struct State {
+    app: AppSurface,
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+    color: uniform::Proxy,
+}
+
+impl Action for State {
+    fn update(&mut self) {
+        self.app.queue.write_buffer(self.color.buffer(0, 0), 0, &[]);
+    }
+    fn new(app: AppSurface) -> Self {
+        let shader = app
+            .device
+            .create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("Shader"),
+                source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+            });
+
+        let color = uniform::Proxy::new(bytemuck::cast_slice(&[1.0; 4]), &[1], &app.device, ShaderStages::FRAGMENT);
+
+        let render_pipeline_layout =
+            app.device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[color.layout()],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline = app
+            .device
+            .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[Vertex::desc()],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: app.config.format.add_srgb_suffix(),
+                        blend: Some(wgpu::BlendState {
+                            color: wgpu::BlendComponent::REPLACE,
+                            alpha: wgpu::BlendComponent::REPLACE,
+                        }),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+                    // or Features::POLYGON_MODE_POINT
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                // If the pipeline will be used with a multiview render pass, this
+                // indicates how many array layers the attachments will have.
+                multiview: None,
+            });
+
+        let vertex_buffer = app
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+        let index_buffer = app
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            });
+        let num_indices = INDICES.len() as u32;
+
+        Self {
+            app,
+            render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            num_indices,
+            color,
+        }
+    }
+
+    fn get_adapter_info(&self) -> wgpu::AdapterInfo {
+        self.app.adapter.get_info()
+    }
+    fn current_window_id(&self) -> WindowId {
+        self.app.view.id()
+    }
+    fn resize(&mut self, size: &PhysicalSize<u32>) {
+        if self.app.config.width == size.width && self.app.config.height == size.height {
+            return;
+        }
+        self.app.resize_surface();
+    }
+    fn request_redraw(&mut self) {
+        self.app.view.request_redraw();
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let (output, view) = self.app.get_current_frame_view(None);
+
+        let mut encoder = self
+            .app
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.set_bind_group(0, self.color.bind_group(0, 0), &[]);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
+        }
+
+        self.app.queue.submit(iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+fn main() {
+    run::<State>(None, None);
+}
